10b128c909448e88570b0a10586f303d24a7eee3
Carlos Eduardo Pereira Alves <carlos.alves@meta.com.br>
2021-04-18 22:13:47 -0300

    refactor: change routes books

--- src/modules/Books/infra/http/routes/books.routes.ts
+++ src/modules/Books/infra/http/routes/books.routes.ts
@@ -6,9 +6,11 @@ const booksRouter = Router()
 
 import BooksController from '../controllers/BooksController'
 import SearchBooksController from '../controllers/SearchBooksController'
+import RentedBooksController from '../controllers/RentedBooksController'
 
 const booksController = new BooksController()
 const searchBooksController = new SearchBooksController()
+const rentedBooksController = new RentedBooksController()
 
 booksRouter.get('/', ensureAuthenticad, booksController.index)
 booksRouter.get('/:isbn', ensureAuthenticad, booksController.show)
@@ -16,5 +18,6 @@ booksRouter.post('/', ensureAuthenticad, booksController.store)
 booksRouter.put('/', ensureAuthenticad, booksController.update)
 booksRouter.delete('/:isbn', ensureAuthenticad, booksController.delete)
 booksRouter.post('/search', ensureAuthenticad, searchBooksController.search)
+booksRouter.post('/rented', rentedBooksController.rented)
 
 export default booksRouter
--- src/modules/Books/infra/http/routes/rented.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import RentedBooksController from '../controllers/RentedBooksController'
-const rentedBooksController = new RentedBooksController()
-
-const rentedRouter = Router()
-
-rentedRouter.post('/', rentedBooksController.create)
-
-export default rentedRouter
